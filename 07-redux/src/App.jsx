fd84e8543933d0c12b87268b22f3a445eff7b006
soheekimdev <soheekim.dev@gmail.com>
2024-10-28 11:53:29 +0900

    ✅ Redux-Thunk 연결

--- 07-redux/src/App.jsx
+++ 07-redux/src/App.jsx
@@ -1,17 +1,20 @@
-/*
-  0. UI 생성
-  1. Action 객체 생성
-  2. Reducer 함수 생성
-  3. 상태 저장소(Store) 생성
-  -------지금까지는 Redux 기능만 사용-------
-  -------이제 React-Redux 사용할 것-------
-  4. App 컴포넌트를 Provider로 감싸기 (main.jsx) => 이제 상태 연결됨
-  5. useSelector 사용해서 상태 가져오기
-  6. useDispatch로 Action 객체 전달하기(사용하기)
+/* Redux 사용 방법
+    0. UI 생성
+    1. Action 객체 생성
+    2. Reducer 함수 생성
+    3. 상태 저장소(Store) 생성
+    -------지금까지는 Redux 기능만 사용-------
+    -------이제 React-Redux 사용할 것-------
+    4. App 컴포넌트를 Provider로 감싸기 (main.jsx) => 이제 상태 연결됨
+    5. useSelector 사용해서 상태 가져오기
+    6. useDispatch로 Action 객체 전달하기(사용하기)
+    -----------Redux-Thunk 설치-----------
+    7. redux-thunk 기능을 가져와서 사용할 수 있도록 미들웨어로 연결
  */
 
+import { legacy_createStore, applyMiddleware, combineReducers } from 'redux';
 import { useDispatch, useSelector } from 'react-redux';
-import combineReducers from '../../04-redux/node_modules/.pnpm/redux@5.0.1/node_modules/redux/src/combineReducers';
+import thunk from 'redux-thunk';
 
 // 1. Action 객체 생성
 const increment1 = {
@@ -53,7 +56,8 @@ const counterReducer2 = (state = 0, action) => {
 const rootReducer = combineReducers({ counterReducer1, counterReducer2 });
 
 // 3. 상태 저장소(Store) 생성
-export const store = createStore(rootReducer); // Reducer를 인자로 전달  *참고: createStore는 이제 사용할 수 없음
+// 7. redux-thunk 기능을 가져와서 사용할 수 있도록 미들웨어로 연결 (createStore에 추가적으로 applyMiddleware 함수를 인자로 전달)
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk)); // Reducer를 인자로 전달  *참고: createStore는 이제 사용할 수 없음
 
 const App = () => {
   // 5. useSelector로 상태 가져오기
@@ -68,7 +72,17 @@ const App = () => {
     <>
       <div>Counter1 : {counter1}</div>
       {/* 6-2. seDispatch로 Action 객체 전달하기(사용하기) - dispatch 함수의 인자로 Action 전달 */}
-      <button onClick={() => dispatch(increment1)}>+</button>
+      <button
+        onClick={() =>
+          dispatch((dispatch) => {
+            setTimeout(() => {
+              dispatch(increment1);
+            }, 1000);
+          })
+        }
+      >
+        +
+      </button>
       <button onClick={() => dispatch(decrement1)}>-</button>
 
       <div>Counter2 : {counter2}</div>
